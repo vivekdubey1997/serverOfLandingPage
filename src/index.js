940a3076fedca377489035c0e09b0e2b747aa0f7
provivek <113443198+vivekdubey1997@users.noreply.github.com>
2024-10-12 11:09:33 +0000

    second

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import {app} from './app.js'
 dotenv.config({
   
 })
-
+console.log(" hi connected to vercel ")
 app.listen(process.env.PORT || 8000, () => {
     console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
 })
